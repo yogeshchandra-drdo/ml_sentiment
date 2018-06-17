96847ac456005846263959e5fe8c55beee9f90db
Yogesh Chandra <yogeshchandra.drdo@gmail.com>
2018-06-17 22:08:01 +0530

    dataset

--- sentiment/sentiment2.py
+++ sentiment/sentiment2.py
@@ -1,35 +1,18 @@
-# $example on$
-from pyspark.ml.feature import HashingTF, IDF, Tokenizer
-# $example off$
-from pyspark.sql import SparkSession
+import numpy as np
+import pandas as pd
 
-print("Sentiment Analysis")
-if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("TfIdfExample")\
-        .getOrCreate()
+print("Sentiment Analysis Project")
+print("--------------------------")
 
-    # $example on$
-    sentenceData = spark.createDataFrame([
-        (0.0, "Hi I heard about Spark"),
-        (0.0, "I wish Java could use case classes"),
-        (1.0, "Logistic regression models are neat")
-    ], ["label", "sentence"])
+#f = open("amazon_cells_labelled.csv")
+#f.readline() # skip the header
+#data = np.loadtxt(f)
 
-    tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
-    wordsData = tokenizer.transform(sentenceData)
+df = pd.read_csv("amazon_cells_labelled.csv", header=1)
+for row in df:
+    print(row)
 
-    hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=20)
-    featurizedData = hashingTF.transform(wordsData)
-    # alternatively, CountVectorizer can also be used to get term frequency vectors
-
-    idf = IDF(inputCol="rawFeatures", outputCol="features")
-    idfModel = idf.fit(featurizedData)
-    rescaledData = idfModel.transform(featurizedData)
-
-    rescaledData.select("label", "features").show()
-
-    # $example off$
-
-    spark.stop()
+#df1 = df.iloc[:,0:1]
+#df1 = df[['sentence']]
+df1 = df[df.columns[0:2]]
+print(df1)
